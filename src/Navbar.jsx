331c2e373a87254d737e75203858e0386d501cc0
Sheriff Aderombi <ayaderombi@gmail.com>
2024-11-20 17:39:18 +0100

    update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,65 +10,59 @@ import logo from './logo.svg';
 
 const Navbar = () => {
 
+
     const [showLinks, setShowLinks] = useState(false);
-    const linksContainerRef = useRef(null);
-    const linksRef = useRef(null);
+    const containerRef = useRef();
+    const linksRef = useRef();
 
     useEffect(() => {
         const linksHeight = linksRef.current.getBoundingClientRect().height;
+
         if (showLinks) {
-            linksContainerRef.current.style.height = `${linksHeight}px`
+            containerRef.current.style.height = `${linksHeight}px`
         } else {
-            linksContainerRef.current.style.height = '0px'
+            containerRef.current.style.height = '0px'
         }
     }, [showLinks])
+
  
   return (
-    <nav>
-      <div className="nav-center">
-        <div className="nav-header">
-            <img src={logo} alt="logo" />
-            <button 
-            className="nav-toggle"
-            onClick={() => {
-                setShowLinks(!showLinks)}}>
-                <FaBars />
-            </button>
-        </div>
-        <div 
-            className="links-container"
-            ref={linksContainerRef}
-        // className="links-container show container"
-            // className={`${
-            //     showLinks ? 'links-container show-container' : 
-            //     'links-container'
-            //     }`}
-                >
-                <ul className="links" ref={linksRef}>
-                    {links.map((link) => {
-                        const {id, url, text} = link 
-                        return (
-                            <li key={id}>
-                                <a href={url}>{text}</a>
-                            </li>
-                        )
-                    })}
+   
+    <>
+        <nav>
+            <div className="nav-center">
+                <div className="nav-header">
+                    <img src={logo} alt="logo" className="logo"/>
+                    <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
+                        <FaBars />
+                    </button>
+                </div>
+                <div className="links-container" ref={containerRef}>
+                    <ul className="links" ref={linksRef}>
+                        {links.map((link) => {
+                            const {id, url, text} = link
+                            return (
+                                <li key={id}>
+                                    <a href={url}>{text}</a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <ul className="social-icons">
+                        {social.map((socialIcon) => {
+                            const {id, url, icon} = socialIcon;
+                            return (
+                                <li key={id}>
+                                    <a href={url}>{icon}</a>
+                                </li>
+                            )
+                        })}
                 </ul>
-        </div>
-        
-        
-        <ul className="social-icons">
-                {social.map((socialIcon) => {
-                    const {id, url, icon} = socialIcon 
-                    return (
-                        <li key={id}>
-                            <a href={url}><img src={icon} alt="" /></a>
-                        </li>
-                    )
-                })}
-        </ul>
-      </div>
-    </nav>
+            </div>
+        </nav>
+    </>
+    
   );
 };
 
